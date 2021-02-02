dc20f99abaee689bc1150abb852e62bcaae124df
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-02-02 14:33:06 -0500

    commit tree d5b8e0d92df06013303d70284339eb9c5c65fe71

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    def initialize(string_url) 
+        @url = string_url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        people = JSON.parse(self.get_response_body)
+        #people.collect {|person| program["agency"]}
+    end
+end
